335fbb110d3fe7445f36578e872b57721005297b
TarakaSivakoti <tarak.sivakoti@gmail.com>
2019-09-12 10:32:43 +0530

    theme specific code

--- src/container.js
+++ src/container.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./App.css";
+import './test.scss';
 import { Row, Col } from "react-bootstrap";
 import pp from "../src/img/pp.jpg";
 import Card1 from "./cards";
 import Table1 from "./table1";
 import Table2 from "./table2";
-import { Button } from "@stardust-ui/react";
+import {Provider,themes, Button } from "@stardust-ui/react";
+import { TeamsThemeContext, getContext, ThemeStyle } from 'msteams-ui-components-react';
+import * as microsoftTeams from "@microsoft/teams-js";
 class Container extends React.Component {
   constructor(props) {
     super(props);
@@ -13,11 +16,84 @@ class Container extends React.Component {
       name: "Chris Naidoos",
       designation: "UX Designer",
       day:12,
-      month:9
+      month:9,
+      theme: "dark",
+      themeStyle: ThemeStyle.teams
     };
   }
-  render() {
+   componentDidMount() {
+    microsoftTeams.initialize();
+    microsoftTeams.getContext((context) => {
+      let theme = context.theme || "";
+      this.updateTheme(theme);
+      this.setState({
+        theme: theme
+      });
+    });
+
+    microsoftTeams.registerOnThemeChangeHandler((theme) => {
+      this.updateTheme(theme);
+      this.setState({
+        theme: theme,
+      }, () => {
+        this.forceUpdate();
+      });
+    });
+  }
+
+   setThemeComponent = () => {
+    if (this.state.theme === "dark") {
+      return (
+        <Provider theme={themes.teamsDark}>
+          <div className="darkContainer">
+            {this.getAppDom()}
+          </div>
+        </Provider>
+      );
+    }
+    else if (this.state.theme === "contrast") {
+      return (
+        <Provider theme={themes.teamsHighContrast}>
+          <div className="highContrastContainer">
+            {this.getAppDom()}
+          </div>
+        </Provider>
+      );
+    } else {
+      return (
+        <Provider theme={themes.teams}>
+          <div className="defaultContainer">
+            {this.getAppDom()}
+          </div>
+        </Provider>
+      );
+    }
+  }
+
+   updateTheme = (theme) => {
+    if (theme === "dark") {
+      this.setState({
+        themeStyle: ThemeStyle.Dark
+      });
+    } else if (theme === "contrast") {
+      this.setState({
+        themeStyle: ThemeStyle.HighContrast
+      });
+    } else {
+      this.setState({
+        themeStyle: ThemeStyle.Light
+      });
+    }
+  }
+
+   getAppDom = () => {
+    const context = getContext({
+      baseFontSize: 10,
+      style: this.state.themeStyle
+    });
+  
     return (
+      <TeamsThemeContext.Provider value={context}>
       <Container-fluid>
         <Row>
           <Col lg="3" md="3" className="col-br">
@@ -46,8 +122,18 @@ class Container extends React.Component {
           </Col>
         </Row>
       </Container-fluid>
+      </TeamsThemeContext.Provider>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        {this.setThemeComponent()}
+      </div>
     );
   }
 }
+
 
 export default Container;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { Provider, themes } from "@stardust-ui/react";
+import { Provider } from "@stardust-ui/react";
 
 ReactDOM.render(
-  <Provider theme={themes.teams}>
+  <Provider>
     <App />
   </Provider>,
   document.getElementById("root")
